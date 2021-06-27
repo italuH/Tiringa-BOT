8bd0691810bd5607cb4cd35523353867c456226c
ItaluH <77984589+italuH@users.noreply.github.com>
2021-06-27 15:49:31 -0300

    Add files via upload

---
+++ src/tebakkata.js
@@ -0,0 +1,198 @@
+[
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "R _ M _ _",
+        "jawaban": "RUMAH"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "T _ _ A _ ",
+        "jawaban": "TAMAN"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "K _ _ A _",
+        "jawaban": "KAMAR"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "T _ G _ _",
+        "jawaban": "TUGAS"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "D _ _ A _ I   _ I _ A   M _ _",
+        "jawaban": "DONASI JIKA MAU"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "B _ T   _ Y   D _ _ I _",
+        "jawaban": "BOT BY DIDIN"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "_ Y _ _ C",
+        "jawaban": "AYANC"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "M _ N _ _",
+        "jawaban": "MANDI"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "_ Y _",
+        "jawaban": "BYE"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "_ O _ _ A _",
+        "jawaban": "ZODIAK"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "P _ _ N _",
+        "jawaban": "POCONG"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "G _ _ P_   B _ _ I",
+        "jawaban": "GEMPA BUMI"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "W _ _ U   _U",
+        "jawaban": "WIBU LU"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "_ A _ N",
+        "jawaban": "DAUN"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "A _ A _ _ S _ S",
+        "jawaban": "ANALISIS"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "_ A _ A",
+        "jawaban": "MATA"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "P _ A _ _ K _",
+        "jawaban": "PRAMUKA"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "M _ _ _ _",
+        "jawaban": "MAKAN"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "T _ _ E",
+        "jawaban": "TAHU"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "K _ N _ O _",
+        "jawaban": "KANTOR"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "B _ T",
+        "jawaban": "BOT"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "D _ _ L _ H",
+        "jawaban": "DAHLAH"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "H _ _ _ R",
+        "jawaban": "HADIR"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "_ ET _ _",
+        "jawaban": "PETIR"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "I _ _ _ NE _ _ _",
+        "jawaban": "INDONESIA"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "_ _ H _ _",
+        "jawaban": "POHON"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "A _ _ O _ -D",
+        "jawaban": "ANBOT-D"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "soal": "D _ _ I _",
+        "jawaban": "DIDIN"
+    }
+}
+]
